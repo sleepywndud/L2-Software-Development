c48c2194ffda57f37accf0222e2814593a3801c2
sleepywndud <Wndx2@duck.com>
2026-03-11 12:05:09 +1300

    .

--- Flask_Stuff/main.py
+++ Flask_Stuff/main.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+WEBSITE_NAME = "Babo Bobo's Pizzaria"
+
 
 @app.route("/")
-def hello_world():
-    return "<p>asdf</p>"
+def main():
+    print("Hello")
 
 
 if __name__ == "__main__":
