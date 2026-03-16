b0cbb882440d5e3c57f3bfd877f51c6e9f2bc7ea
sleepywndud <Wndx2@duck.com>
2026-03-16 15:08:56 +1300

    Button website thingy

--- Flask_Stuff/main.py
+++ Flask_Stuff/main.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 WEBSITE_NAME = "Babo Bobo's Pizzaria"
 
 
-@app.route("/")
+@app.route(
+    "/",
+)
 def main():
-    print("Hello")
+    return render_template("index.html", status="waiting")
+
+
+@app.route("/", methods=["POST"])
+def button1():
+    status = "waiting"
+    if request.method == "POST":
+        if "button1" in request.form:
+            status = "clicked"
+        elif "button2" in request.form:
+            status = "waiting(reset)"
+
+    return render_template("index.html", status=status)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
